d9b12892c7caf94970748c86172599f0949408b8
IsaacKlark <isaackeklarke@gmail.com>
2019-12-27 20:08:18 +0200

    Updates

---
+++ precache-manifest.0215ca696e66c299ce1528e531f809c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3dd65e7f79b8f4317b8a6131146316",
+    "url": "/polydimensional_cube/index.html"
+  },
+  {
+    "revision": "05993e1243f00e546abd",
+    "url": "/polydimensional_cube/static/css/main.dfad44ce.chunk.css"
+  },
+  {
+    "revision": "eae815e6103a2d1e8dd1",
+    "url": "/polydimensional_cube/static/js/2.a339e7e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/polydimensional_cube/static/js/2.a339e7e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05993e1243f00e546abd",
+    "url": "/polydimensional_cube/static/js/main.a552dcb0.chunk.js"
+  },
+  {
+    "revision": "77057ae1584906f3c7c6",
+    "url": "/polydimensional_cube/static/js/runtime-main.0c2f81e2.js"
+  }
+]);
