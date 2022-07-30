ba961eda80c524f82f3ed3e2c4a0cc1506b86555
IsaacKlark <isaackeklarke@gmail.com>
2022-07-30 11:10:34 +0300

    Updates

--- precache-manifest.309927362b2ce0e24becedcb3969422a.js
+++ precache-manifest.309927362b2ce0e24becedcb3969422a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b108a3ce6d6546211d52045a03890d07",
+    "revision": "da81b0f48e863f547a9f0210fc3c561e",
     "url": "/polydimensional_cube/index.html"
   },
   {
-    "revision": "7849e438ef4997d08534",
+    "revision": "6c5b777da2f9359cd359",
     "url": "/polydimensional_cube/static/css/main.9df81f85.chunk.css"
   },
   {
-    "revision": "7648b0a1c95030faeb3d",
-    "url": "/polydimensional_cube/static/js/2.bddc9350.chunk.js"
+    "revision": "217bd36059fbc1fe450e",
+    "url": "/polydimensional_cube/static/js/2.b30496e2.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/polydimensional_cube/static/js/2.bddc9350.chunk.js.LICENSE"
+    "url": "/polydimensional_cube/static/js/2.b30496e2.chunk.js.LICENSE"
   },
   {
-    "revision": "7849e438ef4997d08534",
-    "url": "/polydimensional_cube/static/js/main.b4a5d2af.chunk.js"
+    "revision": "6c5b777da2f9359cd359",
+    "url": "/polydimensional_cube/static/js/main.ac77db8b.chunk.js"
   },
   {
     "revision": "77057ae1584906f3c7c6",
